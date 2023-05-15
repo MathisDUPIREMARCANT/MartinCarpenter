09c0500c4004ed0fd4bf560c63813e9ab39e7ed1
VictorVandevoir <victor.vandevoir@student.junia.com>
2023-05-15 14:34:14 +0200

    CACACACACA

--- Code/C/Function.c
+++ Code/C/Function.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 int Init_boardGame(char* Board){
     
+}
+
+
+int random_number(int maximum) {
+    srand(time(NULL)); // initialiser le générateur de nombres aléatoires avec une graine basée sur le temps actuel
+    return rand() % (maximum + 1); // renvoyer un nombre aléatoire entre 0 et maximum inclus
 }
