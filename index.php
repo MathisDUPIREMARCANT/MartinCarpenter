2b0f290a3d82aa386c355bf43137cfadc5fc5c1c
juliettestm <112863534+juliettestm@users.noreply.github.com>
2023-05-15 11:39:40 +0200

    Construction de l'index
    Début de l'index avec le head

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+session_start() ;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" >
+    
+    <title>Martin Carpenter</title>
+   
+    <meta name="description" content="Martin carpenter Homepages" >
+    <!--page description-->
+    <meta name="keywords" content="Martin Carpenter,Connexion,Rules,Games " >
+    <!--Page keywords-->
+    <meta name="viewport"content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!--Setting up a web page visibility zone for the user-->
+    
+    <meta name="autors" content="Claus,Dupire-Marcant,Lemoine,Saint-Maxent,Tassin,Vandevoir" >
+    <!--Page authors-->
+    <link rel="icon" type="image/x-con" href="../Images/Images.ico/logo.ico" >
+    <!--Browser icon-->
+</head>
+   
