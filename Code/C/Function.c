c41336c87d6e98b711a10206c957e93902184647
MathisDUPIREMARCANT <112871729+MathisDUPIREMARCANT@users.noreply.github.com>
2023-05-15 14:39:07 +0200

    Null => 0

--- Code/C/Function.c
+++ Code/C/Function.c
@@ -6,6 +6,6 @@ int Init_boardGame(char* Board, int x, int y){
 
 
 int Random_number(int maximum) {
-    srand(time(NULL)); // initialiser le générateur de nombres aléatoires avec une graine basée sur le temps actuel
+    srand(time(0)); // initialiser le générateur de nombres aléatoires avec une graine basée sur le temps actuel
     return rand() % (maximum + 1); // renvoyer un nombre aléatoire entre 0 et maximum inclus
 }
