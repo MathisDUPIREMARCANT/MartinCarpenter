f357843749b125d45c9979e006cac5214bc0bd7a
jameslemoine <james.lemoine@student.junia.com>
2023-05-15 16:32:51 +0200

    changement fonction random

--- Code/C/Function.c
+++ Code/C/Function.c
@@ -5,7 +5,7 @@ void Init_boardGame(char* Board, int x, int y){
 }
 
 
-int Random_number(int maximum) {
+int Random(int maximum) {
     srand(time(0)); // initialiser le générateur de nombres aléatoires avec une graine basée sur le temps actuel
     return rand() % (maximum + 1); // renvoyer un nombre aléatoire entre 0 et maximum inclus
 }
@@ -19,3 +19,12 @@ int Place_bridge_on_map(char* Board, int Ymax, int x, int y, int type_bridge){
         *(Board + Ymax*x + y) = '#';
     }
 };
+
+
+int Made_map_test(char* Board, int Ymax, int x, int y, int Nb_ile){
+    *(Board + Ymax*x + y) = 1;
+    int fin = 0;
+    while(fin < Nb_ile){
+        int randp = 
+    }
+}
