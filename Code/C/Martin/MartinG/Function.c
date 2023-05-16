5802189a4c4c028c26a8fcb7c1bc6cbb8f0f0823
MathisDUPIREMARCANT <112871729+MathisDUPIREMARCANT@users.noreply.github.com>
2023-05-16 16:10:13 +0200

    Unique file for map_generator

--- Code/C/Martin/MartinG/Function.c
+++ Code/C/Martin/MartinG/Function.c
@@ -116,41 +116,6 @@ Coord* Next_Coord(Coord* pos, int direction) {
     }
 }
 
-int Map_mading(char* Board, Coord posMax, Coord pos, int Nb_ile) {
-    *(Board + (posMax.y*pos.x) + pos.y) = '1';
-    int end = 0; int Type_bridge = Random(1)+1; int Type_bridge_precedent = 1; int Direction_available[4];int a = 1;
-    while (end < Nb_ile) {
-        int* tab = Space_next_bridge(Board, pos, posMax);
-        for (int i = 0; i < 4; i++) {
-            if (*(tab + i) >= 2) {
-                Direction_available[i] = 1;
-            }
-            else {
-                Direction_available[i] = 0;
-            }
-        }
-        int D_pont;
-        while(a){
-            D_pont = Random(3);
-            if (Direction_available[D_pont] == 1) {
-                a = 0;
-            }
-        }
-        
-        int length = Random(tab[D_pont]);
-        for (int i = 0; i <= length; i++) {
-            Next_Coord(&pos, D_pont);
-            Place_bridge_on_map(Board, posMax, pos, Type_bridge);
-        }
-        
-        Next_Coord(&pos, D_pont);
-        *(Board + posMax.y*pos.x + pos.y) = Type_bridge + Type_bridge_precedent;
-        int Type_bridge = Random(1) + 1;
-        Type_bridge_precedent = Type_bridge;
-        end++;
-    }
-}
-
 void Affichage_board(char* Board, Coord Taille) {
     int i = 0;
     for (i; i < (Taille.x * Taille.y); i++) {
