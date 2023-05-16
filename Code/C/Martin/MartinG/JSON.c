5baa5b6d5a5f4c0fc4d5ef63b78c67c652a24589
VictorVandevoir <victor.vandevoir@student.junia.com>
2023-05-16 16:36:28 +0200

    repush

--- Code/C/Martin/MartinG/JSON.c
+++ Code/C/Martin/MartinG/JSON.c
@@ -4,11 +4,32 @@
 #include <stdlib.h>
 
 void from_C_to_Json_ile(Island ile) {
-	printf("Island : [ x : %d, y : %d, coord : %d,%d, number : %d]", ile.coordo.x, ile.coordo.y, ile.number)
+    printf("Island : [ x : %d, y : %d, coord : %d,%d, number : %d]", ile.pos.x, ile.pos.y, ile.number);
     
 }
 void from_C_to_Json_pont(Bridge pont) {
-    printf("Bridge : [ width : %d, lenght : %d, coord : %d,%d, direction : %c]", pont.size, pont.lenght, pont.coordo.x, pont.coordo.y, pont.direction
+    printf("Bridge : [ width : %d, lenght : %d, coord : %d,%d, direction : %c]", pont.size, pont.lenght, pont.pos.x, pont.pos.y, pont.direction);
 }
 
-void from_C_to_Json(Bridge *liste[], Island
+void from_C_to_Json(Bridge* liste_pont[], Island* liste_ile[], int nb_pont, int nb_ile, int taille[]) {
+	printf("{\n");
+	printf("    \"Island\" : [\n");
+	for (int i = 0; i < nb_ile; i++) {
+		from_C_to_Json_ile(liste_ile[i]);
+		if (i < nb_ile - 1) {
+			printf(",\n");
+		}
+	}
+	printf("    ],\n");
+	printf("    \"Grid\": [\n { \"size\" : [%d, %d] ", taille[0], taille[1]);
+	printf("    ]\n");
+	printf("    \"Bridge\" : [\n");
+	for (int i = 0; i < nb_pont; i++) {
+		from_C_to_Json_pont(liste_pont[i]);
+		if (i < nb_pont - 1) {
+			printf(",\n");
+		}
+	}
+	printf("    ]\n");
+	printf("}\n");
+}
