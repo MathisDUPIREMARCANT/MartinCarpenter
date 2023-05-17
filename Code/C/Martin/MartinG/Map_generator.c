0396276954e04c224caadf2a8c717bd8c24efa96
MathisDUPIREMARCANT <112871729+MathisDUPIREMARCANT@users.noreply.github.com>
2023-05-17 08:59:46 +0200

    type generator upadated

--- Code/C/Martin/MartinG/Map_generator.c
+++ Code/C/Martin/MartinG/Map_generator.c
@@ -56,11 +56,13 @@ int Map_gen(char* Board, Coord posMax, Coord pos, int Nb_island) {
             for (int i = 0; i <= length; i++) {
                 Next_Coord(&pos, D_pont);
                 Place_bridge_on_map(Board, posMax, pos, Type_bridge);
+
+
             }
 
             Next_Coord(&pos, D_pont);
-            *(Board + posMax.x * pos.y + pos.x) = Type_bridge + Type_bridge_precedent;
-            int Type_bridge = Random(1) + 1;
+            Place_island_on_map(Board, posMax, pos, Type_bridge + Type_bridge_precedent);
+            int Type_bridge = Random(1);
             Type_bridge_precedent = Type_bridge;
         end++;
     }
