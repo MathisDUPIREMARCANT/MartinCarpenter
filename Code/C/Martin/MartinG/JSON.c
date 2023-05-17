2013b6ce104490495f36aa6aa8d0a1edf942cbd4
VictorVandevoir <victor.vandevoir@student.junia.com>
2023-05-17 08:58:06 +0200

    changement JSON (insh ca marche)

--- Code/C/Martin/MartinG/JSON.c
+++ Code/C/Martin/MartinG/JSON.c
@@ -3,11 +3,17 @@
 #include <stdlib.h>
 
 void from_C_to_Json_ile(Island ile) {
-    printf("Island : [ x : %d, y : %d, coord : %d,%d, number : %d]", ile.pos.x, ile.pos.y, ile.number);
+    printf("{\"links\" : % d, \"Placement\" : [%d, %d]\n}", ile.pos.x, ile.pos.y, ile.number);
     
 }
 void from_C_to_Json_pont(Bridge pont) {
-    printf("Bridge : [ width : %d, lenght : %d, coord : %d,%d, direction : %c]", pont.size, pont.lenght, pont.pos.x, pont.pos.y, pont.direction);
+	Coord* coordPtr = &pont.pos;
+	
+	printf("{ \n \"width\" : %d, \"lenght\" : %d, \"direction\" : %d, \"Placement\" : [", pont.size, pont.lenght, pont.direction);
+	for (int i = 0; i < pont.lenght; i++) {
+		printf("%d", *(coordPtr + i));
+	}
+	printf("] \n }");
 }
 
 void from_C_to_Json(Bridge* liste_pont, Island* liste_ile, int nb_pont, int nb_ile, int taille[]) {
