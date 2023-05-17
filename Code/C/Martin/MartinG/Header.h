088243e37ac46f95dd3583c53cf278785e655c38
MathisDUPIREMARCANT <112871729+MathisDUPIREMARCANT@users.noreply.github.com>
2023-05-17 19:37:58 +0200

    Ordre alphabétique

--- Code/C/Martin/MartinG/Header.h
+++ Code/C/Martin/MartinG/Header.h
@@ -15,18 +15,20 @@ typedef struct island{
     int number;
 }Island;
 
-void Print_board(char* Board, Coord Taille);
+
+void Free_game(Bridge* bridge, Island* island);
+void From_C_to_Json(Bridge* liste_pont, Island* liste_ile, int nb_pont, int nb_ile, Coord posMax);
+void From_C_to_Json_bridge(Bridge bridge);
+void From_C_to_Json_island(Island island);
 void Place_bridge_on_map(char* Board, Coord posMax, Coord pos, int type_bridge);
 void Place_island_on_map(char* Board, Coord posMax, Coord pos, int weight);
-void From_C_to_Json(Bridge* liste_pont, Island* liste_ile, int nb_pont, int nb_ile, Coord posMax);
-void From_C_to_Json_island(Island island);
-void From_C_to_Json_bridge(Bridge bridge);
-void Free_game(Bridge* bridge, Island* island);
+void Print_board(char* Board, Coord Taille);
+
 
 int Map_gen(char* Board, Coord posMax, Coord pos, int Nb_island);
-int Space_next_bridge(char* Board, Coord pos, Coord posMax, int Direction);
 int Random(int min, int max);
 int Ramification(char* Board, Coord pos, Coord posMax, int Direction, int* Nb_island, int length);
+int Space_next_bridge(char* Board, Coord pos, Coord posMax, int Direction);
 int* Table_copy(int* table, int length);
 
 char* Init_board_Game(Coord pos);
