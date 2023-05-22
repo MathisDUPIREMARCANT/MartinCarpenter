6e6f5b643ce16e311c8354dfa40b6c9b52c7dfd3
juliettestm <112863534+juliettestm@users.noreply.github.com>
2023-05-22 17:19:23 +0200

    Update CSS responsive

---
+++ WEB/settingingames/settinguser.php
@@ -0,0 +1,16 @@
+<?php include("debutsetting.php"); ?>
+<header>
+    <div class="buttonhead">
+        <button class=" back" type=submit>
+            <a href="../user.php">
+                <img class="backimg" src="../image/arrow.png">
+            </a>
+        </button>
+        <button class="user">
+            <a href="../user.php">
+                <img class="userimg" src="../image/martin.png">
+            </a>
+        </button>
+    </div>
+</header>
+<?php include("finsetting.php"); ?>
