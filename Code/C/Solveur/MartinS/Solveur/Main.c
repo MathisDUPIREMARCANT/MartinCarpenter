221f888039a81277480fd36f6c82c04bba679b42
jameslemoine <james.lemoine@student.junia.com>
2023-05-24 11:25:37 +0200

    Island_on_map

--- Code/C/Solveur/MartinS/Solveur/Main.c
+++ Code/C/Solveur/MartinS/Solveur/Main.c
@@ -1,6 +1,18 @@
+#include "Header.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-
-
+#define X 5
+#define Y 7
 void main() {
+
+
+
+	Coord posMax = { Y, X }; //{ *(argv[1]), *(argv[2])} 
+	Coord pos = { Random(0, posMax.x), Random(0, posMax.y) };
+	//
+	char* board[Y * X] = { "*****1~3****#****4****#****4#2*****" };
+
+	solveur(board, pos, posMax);
 
 }
