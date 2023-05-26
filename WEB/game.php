0e84a711d9a7f38e77bd4197f26d221fd155b81d
clausdamia <112809972+clausdamia@users.noreply.github.com>
2023-05-26 10:34:05 +0200

    update

--- WEB/game.php
+++ WEB/game.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+if(isset($_SESSION['username'])){
+    echo'
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -38,18 +43,18 @@
         </div>
     </header>
     <main class="main">
-        <button class="Buttonstory" onclick="redirectTo('storymod.php')"><a class="textstory">Story
+        <button class="Buttonstory" onclick="redirectTo("storymod.php")"><a class="textstory">Story
                 Mod</a></button>
         <div class="games">
-            <button class="Buttoneasy" onclick="redirectTo('randommod.php')"><a class="texteasy">Easy Mod</a></button>
-            <button class="Buttonmedium" onclick="redirectTo('randommod.php')"><a class="textmedium">Medium
+            <button class="Buttoneasy" onclick="redirectTo("randommod.php")"><a class="texteasy">Easy Mod</a></button>
+            <button class="Buttonmedium" onclick="redirectTo("randommod.php")"><a class="textmedium">Medium
                     Mod</a></button>
-            <button class="Buttonhard" onclick="redirectTo('randommod.php')"><a class="texthard">Hard Mod</a></button>
+            <button class="Buttonhard" onclick="redirectTo("randommod.php")"><a class="texthard">Hard Mod</a></button>
         </div>
         <div class="creationgames">
-            <button class="Buttoncreate" onclick="redirectTo('createmod.php')"><a class="textcreate">Create
+            <button class="Buttoncreate" onclick="redirectTo("createmod.php")"><a class="textcreate">Create
                     Mod</a></button>
-            <button class="ButtonLevel" onclick="redirectTo('level.php')"><a class="textlevel">My Level</a></button>
+            <button class="ButtonLevel" onclick="redirectTo("level.php")"><a class="textlevel">My Level</a></button>
         </div>
 
 
@@ -62,4 +67,10 @@
     </script>
 </body>
 
-</html>
+</html>';
+}
+else{ 
+    header("Location: sign_in_up.php?fail=2");
+    return;
+}
+?>
