812fa2a214c390c4e176f01bf6bd75bc120c30a0
MathisDUPIREMARCANT <112871729+MathisDUPIREMARCANT@users.noreply.github.com>
2023-05-26 14:08:54 +0200

    Test

---
+++ Code/C/Solveur/MartinS/Solveur/Mathis.c
@@ -0,0 +1,16 @@
+#include "Header.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void solver(char* Board, Coord posMax, Coord pos, int Direction[4]) {
+
+	if (Direction != NULL) {
+
+		Coord Copy_pos;
+		
+		for (int i = 0; i < 4; i++) {
+			Copy_pos = { pos.x, pos.y };
+		}
+	}
+}
