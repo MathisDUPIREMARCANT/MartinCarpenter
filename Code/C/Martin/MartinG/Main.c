c81dc2790d6c579833b9338147e8b7642dbaaf0e
MathisDUPIREMARCANT <112871729+MathisDUPIREMARCANT@users.noreply.github.com>
2023-06-02 09:56:58 +0200

    Correction G

--- Code/C/Martin/MartinG/Main.c
+++ Code/C/Martin/MartinG/Main.c
@@ -12,13 +12,13 @@
 void main(int argc, char *argv[]) {
 
 	srand(time(NULL));
-	int nombre_iles = atoi(argv[1]);
-	int nombre_x = atoi(argv[2]);
-	int nombre_y = atoi(argv[3]);
+	//int nombre_iles = atoi(argv[1]);
+	//int nombre_x = atoi(argv[2]);
+	//int nombre_y = atoi(argv[3]);
 
-	//int nombre_iles = 7;
-	//int nombre_x = 7;
-	//int nombre_y = 9;
+	int nombre_iles = 5;
+	int nombre_x = 5;
+	int nombre_y = 5;
 
 	Coord posMax = { nombre_x, nombre_y};
 	Coord pos = { Random(0, posMax.x), Random(0, posMax.y) };
@@ -30,12 +30,13 @@ void main(int argc, char *argv[]) {
 	//printf("\n\nTest : %d\n", test);
 
 	while (test == -1) { 
+		free(Board);
 		Board = Init_board_Game(posMax);
 		test = Map_gen(Board, posMax, pos, nombre_iles);
 		//printf("\n\nTest : %d\n", test);
 	}
 
 	Print_board(Board, posMax);
-	//free(Board);
+	free(Board);
 
 }
