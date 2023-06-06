ce59a3f9bbfada5b50f6917db0fe810982c77009
MathisDUPIREMARCANT <112871729+MathisDUPIREMARCANT@users.noreply.github.com>
2023-06-06 17:38:01 +0200

    Save solveur almost done

--- Code/C/Solveur/MartinS/Solveur/Main.c
+++ Code/C/Solveur/MartinS/Solveur/Main.c
@@ -1,47 +1,65 @@
 #include "Header.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+#define _CRT_SECURE_DEPRECATE_MEMORY
+#include <memory.h>
 
 // -*- coding: utf-8 -*-
 
 
-#define X 11
-#define Y 7
+#define X 7
+#define Y 9
 void main() {
 
 	Coord posMax = { X, Y }; //{ *(argv[1]), *(argv[2])} 
 	Coord pos;
-	
-	Bridge** Result = (Bridge**)malloc(sizeof(Bridge*));
 
-	char Board[X * Y +1] = "1*****4*4*1****************************2*1***************2***3****************";
+	Bridge** Result = (Bridge**)malloc(sizeof(Bridge*) * 10);
+
+	//char Board[X * Y] = { "1*****4*4*1****************************2*1***************2***3****************" };
 	//char Board[X * Y] = { "*1*2*****************3*5*******1*2*" };
+
+	char Board[X * Y + 1];
+	//strncpy_s(Board, _countof(Board), "*************2*5********2********14*2***************2*8****5*1*4**********************3*2***********************1*3***2***********************1******4**22**************", _TRUNCATE);
+	strncpy_s(Board, _countof(Board), "*4**3*****************2**1*************************************", _TRUNCATE);
+
 	pos.x = 0;
 	pos.y = 0;
 
 	int Nb_solution = 0;
 	int* Pt_solution = &Nb_solution;
 
-	int Nb_bridge;
-	Nb_bridge = 0;
+	int Nb_bridge = 0;// = (int*)malloc(sizeof(int));
 
-	int Nb_island = 0;
+	int Nb_bridge_max = 0;
+	int* Pt_max = &Nb_bridge_max;
+
+	int Nb_island = Island_on_map(Board, pos, posMax);;
 	int* Pt_island = &Nb_island;
 
-	Bridge* Bridges = (Bridge*)malloc(sizeof(Bridge*));
-	Nb_island = Island_on_map(Board, pos, posMax);
-	Island* Islands = (Island*)malloc(Nb_island * sizeof(Island*));
+	Bridge** Pt_bridges;
+	Bridge* Bridges = (Bridge*)malloc(sizeof(Bridge));
+	Island* Islands = (Island*)malloc(Nb_island * sizeof(Island));
 
+	Pt_bridges = &Bridges;
 
 	Stock_island(Islands, posMax, Board);
-	*(Board + 77 )= 0;
-	Solver(Result, Board, posMax, pos, NULL, Pt_solution, Bridges, Nb_bridge);
-	Print_board(Board, posMax);
-	
-	
-	if (Result != NULL ){//&& Nb_solution) {
-		Print_board(Board, posMax);
-		//From_C_to_Json(Bridges, Islands, Nb_bridge, Nb_island, posMax, *Pt_solution);
-		From_C_to_Json(Bridges, Islands, Nb_bridge, Nb_island, posMax, 1);
+
+	Solver(Result, Board, posMax, pos, NULL, Pt_solution, Pt_max, Nb_bridge, Pt_bridges);
+
+	printf("[");
+	for (int i = 0; i < Nb_solution; i++) {
+		From_C_to_Json(*(Result + i), Islands, Nb_bridge_max, Nb_island, posMax, Nb_solution);
+		if (i != Nb_solution - 1) {
+			printf(",");
+		}
 	}
+	printf("]");
+	//Print_board(Board, posMax);
+	//if (Result != NULL && Nb_solution) {
+	//	//Print_board(*Result, posMax);
+	//	//From_C_to_Json(Bridges, Islands, Nb_bridge, Nb_island, posMax);
+	//}
 }
